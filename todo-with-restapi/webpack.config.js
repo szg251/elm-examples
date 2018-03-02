52f3cf872555dd5336eb7a1137bf37f238c0c681
Szabo Gergely <gszabo@yahoo-corp.jp>
2018-03-02 21:21:00 +0900

    [change] adding uglifyjs to webpack

--- todo-with-restapi/webpack.config.js
+++ todo-with-restapi/webpack.config.js
@@ -1,51 +1,54 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path                  = require('path');
+const CleanWebpackPlugin    = require('clean-webpack-plugin');
+const HtmlWebpackPlugin     = require('html-webpack-plugin');
+const UglifyJSWebpackPlugin = require('uglifyjs-webpack-plugin');
+
+const cleanPlugin    = new CleanWebpackPlugin('dist');
+const htmlPlugin     = new HtmlWebpackPlugin({title : 'Todo App', template : 'src/index.html'});
+const uglifyjsPlugin = new UglifyJSWebpackPlugin();
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    entry: {
-        app: './src/index.js',
+    entry : {
+        app : './src/index.js',
     },
 
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name].js',
+    output : {
+        path : path.join(__dirname, 'dist'),
+        filename : '[name].js',
     },
 
-    module: {
-        rules: [
+    module : {
+        rules : [
             {
-                test: /\.elm$/,
-                exclude: [
+                test    : /\.elm$/,
+                exclude : [
                     path.join(__dirname, 'src/elm-stuff'),
                     path.join(__dirname, 'node_modules'),
                 ],
-                use: {
+                use : {
                     loader: 'elm-webpack-loader',
-                }
+                    options: {
+                        debug: !isProduction,
+                    },
+                },
             },
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader' ]
+                test : /\.css$/,
+                use  : ['style-loader', 'css-loader' ],
             },
         ],
 
-        noParse: /\.elm$/,
+        noParse : /\.elm$/,
     },
 
-    plugins: [
-        new CleanWebpackPlugin('dist'),
-        new HtmlWebpackPlugin({
-            title: 'Todo App',
-            template: 'src/index.html',
-        }),
-    ],
+    plugins: isProduction
+        ? [cleanPlugin, htmlPlugin, uglifyjsPlugin]
+        : [htmlPlugin]
+    ,
 
-    devServer: {
-        port: 8000,
-        inline: true,
-        stats: { colors: true },
-        contentBase: path.join(__dirname, 'dist'),
-    }
-
+    devServer : {
+        port : 8000,
+    },
 }
