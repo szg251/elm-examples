2eb059b27e8c0a8d25a7df0b48144cf2658083ea
Szabo Gergely <gszabo@yahoo-corp.jp>
2018-03-03 16:28:01 +0900

    [add] some more tests

--- todo-with-restapi/tests/AppTest.elm
+++ todo-with-restapi/tests/AppTest.elm
@@ -13,7 +13,9 @@ import RemoteData
 
 stubTodos : List Todo
 stubTodos =
-    [ { id = "a", value = "vala", done = False } ]
+    [ { id = "1", value = "valb", done = True }
+    , { id = "0", value = "vala", done = False }
+    ]
 
 
 stubModel : Model
@@ -27,19 +29,81 @@ stubModel =
 -- TESTS
 
 
-myfirsttest : Test
-myfirsttest =
-    describe "Update function"
-        [ test "Submit a todo" <|
-            \_ ->
-                let
-                    expectedTodos =
-                        RemoteData.succeed
-                            [ { id = "0", value = "newtodo", done = False }
-                            , { id = "a", value = "vala", done = False }
-                            ]
-                in
-                    update (SubmitTodo "newtodo") stubModel
-                        |> Tuple.first
-                        |> Expect.equal { stubModel | todos = expectedTodos }
+updateTests : Test
+updateTests =
+    describe "Update"
+        [ testSubmitTodo
+        , testDelTodo
+        , testToggleTodo
+        , testAfterPutNewTodo
         ]
+
+
+testSubmitTodo : Test
+testSubmitTodo =
+    test "SubmitTodo" <|
+        \_ ->
+            let
+                expectedTodos =
+                    RemoteData.succeed
+                        [ { id = "2", value = "newtodo", done = False }
+                        , { id = "1", value = "valb", done = True }
+                        , { id = "0", value = "vala", done = False }
+                        ]
+            in
+                update (SubmitTodo "newtodo") stubModel
+                    |> Tuple.first
+                    |> Expect.equal { stubModel | todos = expectedTodos }
+
+
+testDelTodo : Test
+testDelTodo =
+    test "DelTodo" <|
+        \_ ->
+            let
+                expectedTodos =
+                    RemoteData.succeed
+                        [ { id = "0", value = "vala", done = False } ]
+            in
+                update (TodoMsg << DelTodo <| "1") stubModel
+                    |> Tuple.first
+                    |> .todos
+                    |> Expect.equal expectedTodos
+
+
+testToggleTodo : Test
+testToggleTodo =
+    test "ToggleTodo" <|
+        \_ ->
+            let
+                expectedTodos =
+                    RemoteData.succeed
+                        [ { id = "1", value = "valb", done = True }
+                        , { id = "0", value = "vala", done = True }
+                        ]
+            in
+                update (TodoMsg << ToggleTodo <| "0") stubModel
+                    |> Tuple.first
+                    |> .todos
+                    |> Expect.equal expectedTodos
+
+
+testAfterPutNewTodo : Test
+testAfterPutNewTodo =
+    test "AfterPutNewTodo" <|
+        \_ ->
+            let
+                expectedTodos =
+                    RemoteData.succeed
+                        [ { id = "newid", value = "valb", done = True }
+                        , { id = "0", value = "vala", done = False }
+                        ]
+
+                newtodo =
+                    RemoteData.succeed
+                        { id = "newid", value = "valb", done = True }
+            in
+                update (ApiMsg (AfterPutNewTodo "1" newtodo)) stubModel
+                    |> Tuple.first
+                    |> .todos
+                    |> Expect.equal expectedTodos
