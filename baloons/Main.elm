41c8cdb141a2698e32a404c78ea6a33ffc6476b6
Szabo Gergely <gszabo@yahoo-corp.jp>
2018-04-04 21:35:16 +0900

    [add] canvas resizes with window

--- baloons/Main.elm
+++ baloons/Main.elm
@@ -6,10 +6,15 @@ import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Mouse exposing (Position)
 import Time exposing (Time, second)
+import Window exposing (Size)
+import Task
 
 
 type alias Model =
-    { circles : List Baloon, newBaloon : Maybe Baloon }
+    { baloons : List Baloon
+    , newBaloon : Maybe Baloon
+    , windowSize : Size
+    }
 
 
 type alias Baloon =
@@ -24,6 +29,7 @@ type Msg
     | MouseDown Position
     | MouseUp Position
     | Tick Time
+    | WindowResize Size
 
 
 fallDown : Baloon -> Baloon
@@ -40,19 +46,19 @@ fallDown baloon =
         { baloon | position = newPosition, speed = newSpeed }
 
 
-outOfScreen : List Baloon -> List Baloon
-outOfScreen baloons =
-    List.filter (\{ position } -> position.x <= 1000 && position.y <= 1000) baloons
+outOfScreen : Size -> List Baloon -> List Baloon
+outOfScreen window baloons =
+    List.filter (\{ position } -> position.y < window.height) baloons
 
 
 initmodel : Model
 initmodel =
-    { circles = [], newBaloon = Nothing }
+    { baloons = [], newBaloon = Nothing, windowSize = { width = 0, height = 0 } }
 
 
-init : ( Model, Cmd msg )
+init : ( Model, Cmd Msg )
 init =
-    initmodel ! [ Cmd.none ]
+    initmodel ! [ Task.perform WindowResize Window.size ]
 
 
 update : Msg -> Model -> ( Model, Cmd msg )
@@ -61,13 +67,16 @@ update msg model =
         NoOp ->
             model ! [ Cmd.none ]
 
+        WindowResize size ->
+            { model | windowSize = size } ! [ Cmd.none ]
+
         MouseDown position ->
             { model | newBaloon = Just { position = position, speed = -5, size = 5 } }
                 ! [ Cmd.none ]
 
         MouseUp position ->
             { model
-                | circles = Maybe.Extra.toList (model.newBaloon) ++ model.circles
+                | baloons = Maybe.Extra.toList model.newBaloon ++ model.baloons
                 , newBaloon = Nothing
             }
                 ! [ Cmd.none ]
@@ -78,7 +87,7 @@ update msg model =
                     { baloon | size = baloon.size + 1 }
             in
                 { model
-                    | circles = List.map fallDown model.circles |> outOfScreen
+                    | baloons = List.map fallDown model.baloons |> outOfScreen model.windowSize
                     , newBaloon = Maybe.map inflate model.newBaloon
                 }
                     ! [ Cmd.none ]
@@ -90,18 +99,12 @@ subscriptions model =
         [ Mouse.downs MouseDown
         , Mouse.ups MouseUp
         , Time.every (second / 24) Tick
+        , Window.resizes WindowResize
         ]
 
 
 view : Model -> Html Msg
 view model =
-    svg
-        [ width "1000", height "1000" ]
-        (viewCircles (Maybe.Extra.toList model.newBaloon ++ model.circles))
-
-
-viewCircles : List Baloon -> List (Svg Msg)
-viewCircles circles =
     let
         drawCircle : Baloon -> Svg Msg
         drawCircle { position, size } =
@@ -113,7 +116,11 @@ viewCircles circles =
                 ]
                 []
     in
-        List.map drawCircle circles
+        svg
+            [ width (toString model.windowSize.width)
+            , height (toString model.windowSize.height)
+            ]
+            (List.map drawCircle model.baloons)
 
 
 main =
